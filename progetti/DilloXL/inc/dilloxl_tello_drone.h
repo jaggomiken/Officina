6678e2bdc281f4aa735c29c49c75da9fa76d881a
Michele Iacobellis <m.iacobellis@planetek.it>
2024-12-11 19:04:31 +0100

    [DILLOXL]: GPLv3 come licenza su tutto.

--- progetti/DilloXL/inc/dilloxl_tello_drone.h
+++ progetti/DilloXL/inc/dilloxl_tello_drone.h
@@ -1,7 +1,23 @@
 /* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
  * DILLO EXTRA LARGE - DILLOXL
- * (C) Copyright by Michele Iacobellis
+ * (C) 2024 Copyright by Michele Iacobellis
  * A project for students...
+ * 
+ * This file is part of DILLOXL.
+ *
+ * DILLOXL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DILLOXL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DILLOXL. If not, see <http://www.gnu.org/licenses/>.
+ * 
  * <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
 #ifndef dilloxl_tello_drone_h
 #define dilloxl_tello_drone_h
